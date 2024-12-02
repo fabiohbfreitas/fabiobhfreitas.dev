9fc370d7cb6c6182196c3708fe6c81a4ad9182fa
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-12-02 17:48:05 -0300

    fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-slate-800">
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-700 text-slate-400">
       <pre className="font-mono">
         <code>
           soon™
