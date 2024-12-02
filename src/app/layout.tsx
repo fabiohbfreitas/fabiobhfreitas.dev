6adfe58fe3b2568976f9adac45f167775d9a7257
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-12-02 15:23:01 -0300

    fix title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Fabio H. B. Fretias",
+  title: "Fábio Freitas | Desenvolvedor iOS",
   description: "Portifolio de Fábio Freitas Desenvolvedor iOS",
 };
 
